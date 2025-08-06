d1c11c3ded8f860a0a4a313d0c9388cd6549b21d
Matias <matiasch256@gmail.com>
2025-08-06 16:12:42 -0300

    refactor(database): Sanitizar variables de entorno de la conexión

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,10 +1,10 @@
 import mssql from "mssql";
 
 const config = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  server: process.env.DB_SERVER,
-  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER?.trim(),
+  password: process.env.DB_PASSWORD?.trim(),
+  server: process.env.DB_SERVER?.trim(),
+  database: process.env.DB_DATABASE?.trim(),
   options: {
     encrypt: false,
     trustServerCertificate: true,
